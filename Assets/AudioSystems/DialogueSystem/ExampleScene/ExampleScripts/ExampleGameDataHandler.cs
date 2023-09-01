8ef07076d5e3c9b61c7761a3fed6609aaaa73171
samsquanchone <105938519+samsquanchone@users.noreply.github.com>
2023-09-01 15:47:20 +0100

    Arranging files and scenes for submission
    Re-arranged files into systems that are easily accessible and obvious which are main scripts, example scripts, third party scripts and example scenes

---
+++ Assets/AudioSystems/DialogueSystem/ExampleScene/ExampleScripts/ExampleGameDataHandler.cs
@@ -0,0 +1,31 @@
+using DialogueSystem.DataResolver;
+using UnityEngine;
+
+public class ExampleGameDataHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Creating new game data objects
+        GameDataValue test = new((uint)200);
+        GameDataValue questAcceptedBool = new((bool)false);
+        GameDataValue spokenToBenBool = new((bool)false);
+        GameDataValue questCompletedBool = new((bool)false);
+        GameDataValue questTurnedIntoJohn = new((bool)false);
+        GameDataValue questTurnedIntoDora = new((bool)false);
+        GameDataValue spokenToDoraBool = new((bool)false);
+
+        //Adding the objects to this scenes game data hash table (In the game data resolver class)
+        GameDataResolver.Instance.AddGameDataToHashTable((uint)1, test);
+        GameDataResolver.Instance.AddGameDataToHashTable((uint)2, questAcceptedBool);
+        GameDataResolver.Instance.AddGameDataToHashTable((uint)3, spokenToBenBool);
+        GameDataResolver.Instance.AddGameDataToHashTable((uint)4, questCompletedBool);
+        GameDataResolver.Instance.AddGameDataToHashTable((uint)5, questTurnedIntoJohn);
+        GameDataResolver.Instance.AddGameDataToHashTable((uint)6, questTurnedIntoDora);
+        GameDataResolver.Instance.AddGameDataToHashTable((uint)7, spokenToDoraBool);
+
+        //Set a game data value from the created hashtable 
+        GameDataResolver.Instance.SetGameDataVariable((uint)1, 25);
+    }
+
+}
